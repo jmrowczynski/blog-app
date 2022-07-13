951980349aba92e0b653ada389bbe1ed768e81de
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2022-07-13 23:49:20 +0200

    set application bar depending on user login token

--- src/components/ApplicationBar.js
+++ src/components/ApplicationBar.js
@@ -1,21 +1,31 @@
 import { AppBar, Box, Button, Toolbar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { login } from '../routing/routes';
+import { useAppContext } from '../context/app.context';
 
 const ApplicationBar = () => {
+    const { token } = useAppContext();
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="fixed">
                 <Toolbar>
-                    <Button
-                        color="inherit"
-                        style={{ marginLeft: 'auto' }}
-                        component={Link}
-                        to={login}
-                    >
-                        Login
-                    </Button>
-                    <Button color="inherit">Register</Button>
+                    {!token ? (
+                        <>
+                            <Button
+                                color="inherit"
+                                style={{ marginLeft: 'auto' }}
+                                component={Link}
+                                to={login}
+                            >
+                                Login
+                            </Button>
+                            <Button color="inherit">Register</Button>
+                        </>
+                    ) : (
+                        <Button color="inherit" style={{ marginLeft: 'auto' }}>
+                            Account
+                        </Button>
+                    )}
                 </Toolbar>
             </AppBar>
         </Box>
