6e21c5b9dc1d6a4d07989bc57067dc29f84ee678
Jakub Mrówczyński <jmrowczynski12@gmail.com>
2022-11-06 23:53:31 +0100

    clear query client when logout

--- src/services/api/hooks/useLogoutMutation.ts
+++ src/services/api/hooks/useLogoutMutation.ts
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 import AuthApi from '../connections/AuthApi';
 import { useAppContext } from '../../../context/app.context';
 import { home } from '../../../routing/routes';
@@ -9,10 +9,12 @@ export const useLogoutMutation = () => {
     const { removeUser } = useAppContext();
     const navigate = useNavigate();
     const { enqueueSnackbar } = useSnackbar();
+    const queryClient = useQueryClient();
 
     return useMutation(() => AuthApi.logout(), {
         onSuccess() {
             removeUser();
+            queryClient.clear();
             enqueueSnackbar('Logged out!', { variant: 'success' });
             navigate(home, { replace: true });
         },
